fb584f3811163cd6df9d88dedfdc7d4d7f6948a7
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2011-09-22 10:59:10 -0500

    Kittybot's arrival is now in a function to avoid having a million callbacks

--- kitty.js
+++ kitty.js
@@ -3,13 +3,12 @@ var ranger = require('ranger');
 
 console.log('Meow. Is it is me you are looking for?');
 
-var client = ranger.createClient("fellowshiptech","52d397ade97cfbfc96d58b50a00996842d785cd7");
-client.room(438825, function(room) {
-  room.join(function() {
-    room.listen(function(message) {
-      if(message.type === "TextMessage" && message.body.match("Bot")) {
-        room.speak("Bot is here to serve.");
-      }
+
+function botArrival() {
+  var client = ranger.createClient("fellowshiptech","52d397ade97cfbfc96d58b50a00996842d785cd7");
+  client.room(438825, function(room) {
+    room.join(function() {
+      room.speak("Bot is here.");
     });
   });
-});
+}
