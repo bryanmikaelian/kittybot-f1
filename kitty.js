cd18b871e5c466962b618beca6f4a8c9fab0c89d
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2011-09-21 22:29:29 -0500

    More Node.js fun.

--- kitty.js
+++ kitty.js
@@ -1,8 +1,16 @@
 var http = require('http');
 
+function kittybotrespond() {
+  console.log('Kittybot has sent a response.');
+}
+
 var server = http.createServer(function(req, res){
   res.write('Hi there.\n');
+
+  setTimeout(kittybotrespond, 3000);
+
   res.end();
 }).listen(8100);
+
 
 console.log('Meow. Is it is me you are looking for?');
