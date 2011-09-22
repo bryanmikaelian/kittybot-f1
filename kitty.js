83e3744fe74935f089da0ded6ce084ae86851043
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2011-09-22 16:34:55 -0500

    Reverted

--- kitty.js
+++ kitty.js
@@ -5,14 +5,10 @@ server = http.createServer(function(req, res) {
   res.writeHead(200, {
     'Content-Type': 'text/plain'
   });
-
-  console.log(ranger);
-
-   return res.end("Meow.\n");
+  return res.end("Meow.\n");
 });
 
 port = process.env.PORT || 3000;
 server.listen(port);
-
-
 console.log('Meow. Is it is me you are looking for?');
+
