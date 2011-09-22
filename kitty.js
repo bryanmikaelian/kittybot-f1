3e1ca7de8d4d6965f5391fc858a385ebd250556e
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2011-09-22 11:40:12 -0500

    Ditched chinese. ALso, Kittybot will now suggest a place every 24 hours, from 11:00 AM each day

--- kitty.js
+++ kitty.js
@@ -3,7 +3,7 @@ var ranger = require('ranger');
 
 /* Pick a random place to eat */
 function randomPlaceToEat() {
-  var placesToEat = ['Chipotle', 'Jimmy Johns', 'Chick Fil A', 'Whataburger', 'Firehouse Subs', 'Chinese', 'Urban Eatz', 'Wendy\'s', 'Jersey Mikes', 'Five Guys'];
+  var placesToEat = ['Chipotle', 'Jimmy Johns', 'Chick Fil A', 'Whataburger', 'Firehouse Subs',  'Urban Eatz', 'Wendy\'s', 'Jersey Mikes', 'Five Guys'];
   var picker = Math.floor(Math.random() * 11);
   var client = ranger.createClient("fellowshiptech","52d397ade97cfbfc96d58b50a00996842d785cd7");
   client.room(438825, function(room) {
@@ -13,7 +13,8 @@ function randomPlaceToEat() {
   });
 }
 
-setTimeout(randomPlaceToEat, 1000);
+// Once per day at 11:00 AM
+setTimeout(randomPlaceToEat, 84277222);
 
 console.log('Meow. Is it is me you are looking for?');
 
