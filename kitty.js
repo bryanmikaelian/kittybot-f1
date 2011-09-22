cf455a2097224163172080d486a8af3e5667fb22
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2011-09-22 16:44:37 -0500

    Added a setTimeoutcall to specify that kittybot is listening to a room.

--- kitty.js
+++ kitty.js
@@ -10,14 +10,18 @@ server = http.createServer(function(req, res) {
 
 port = process.env.PORT || 3000;
 server.listen(port);
-console.log('Meow. Is it is me you are looking for?');
-
-client = ranger.createClient("fellowshiptech", "52d397ade97cfbfc96d58b50a00996842d785cd7");
-client.room(438825, function(room) {
-  room.join(function() {
-    room.listen(function() {
-      console.log("Kittybot is listening in the room: " + room.name);
+setTimeout(function() {
+  client = ranger.createClient("fellowshiptech", "52d397ade97cfbfc96d58b50a00996842d785cd7");
+  client.room(438825, function(room) {
+    room.join(function() {
+      room.listen(function() {
+        console.log("Kittybot is listening in the room: " + room.name);
+      });
     });
   });
-});
+}, 1000);
+
+console.log('Meow. Is it is me you are looking for?');
+
+
 
