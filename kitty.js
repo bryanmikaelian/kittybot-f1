23af55ba55e6c3734a9d128be5fa2cc154d1bc90
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2011-09-21 23:52:35 -0500

    Kittybot will now response to someone saying Bot.  There is a bug where it keeps spamming its message though.

--- kitty.js
+++ kitty.js
@@ -1,21 +1,15 @@
 var http = require('http');
 var ranger = require('ranger');
 
-function kittybotrespond() {
-  var client = ranger.createClient("fellowshiptech", "52d397ade97cfbfc96d58b50a00996842d785cd7");
-  client.room(438825, function(room) {
-    room.speak("hello world\n");
-  });
-}
-
-var server = http.createServer(function(req, res){
-  res.write('Hi there.\n');
-
-  kittybotrespond();
-
-  res.end();
-}).listen(8100);
-
-
 console.log('Meow. Is it is me you are looking for?');
 
+var client = ranger.createClient("fellowshiptech","52d397ade97cfbfc96d58b50a00996842d785cd7");
+client.room(438825, function(room) {
+  room.join(function() {
+    room.listen(function(message) {
+      if(message.type === "TextMessage" && message.body.match("Bot")) {
+        room.speak("Bot is here to serve.");
+      }
+    });
+  });
+});
