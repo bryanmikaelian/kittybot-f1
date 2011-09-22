5ecd3ae58b1ab68aba84166f4bd49767b0062a88
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2011-09-22 11:41:00 -0500

    Smarter math.

--- kitty.js
+++ kitty.js
@@ -4,7 +4,7 @@ var ranger = require('ranger');
 /* Pick a random place to eat */
 function randomPlaceToEat() {
   var placesToEat = ['Chipotle', 'Jimmy Johns', 'Chick Fil A', 'Whataburger', 'Firehouse Subs',  'Urban Eatz', 'Wendy\'s', 'Jersey Mikes', 'Five Guys'];
-  var picker = Math.floor(Math.random() * 11);
+  var picker = Math.floor(Math.random() * (placesToEat.length - 1));
   var client = ranger.createClient("fellowshiptech","52d397ade97cfbfc96d58b50a00996842d785cd7");
   client.room(438825, function(room) {
     room.join(function() {
