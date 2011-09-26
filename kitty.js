0152e92db13f72fb2e0f81dfd10fa26e3494d10e
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2011-09-26 12:19:19 -0500

    Starting to refactor

--- kitty.js
+++ kitty.js
@@ -1,14 +1,7 @@
 var http = require('http');
+var client = require('ranger').createClient("fellowshiptech", "52d397ade97cfbfc96d58b50a00996842d785cd7");
 
 console.log('Meow. Is it is me you are looking for?');
-
-server = http.createServer(function(req, res) {
-  res.writeHead(200, {
-    'Content-Type': 'text/plain'
-  });
-  return res.end("Meow\n");
+client.room(439862, function(room) {
+  room.listen(function(message) { console.log("User ID" + message.userId + " said " + message.body); });
 });
-
-port = process.env.PORT || 3000;
-server.listen(port);
-
