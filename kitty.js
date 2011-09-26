fe06bcb8a392f9938777f9623c49b915183e3852
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2011-09-26 15:08:14 -0500

    That's what she said, Matthew

--- kitty.js
+++ kitty.js
@@ -1,10 +1,12 @@
 var http = require('http');
-var client = require('ranger').createClient("fellowshiptech", "52d397ade97cfbfc96d58b50a00996842d785cd7");
+var client = require('ranger').createClient("fellowshiptech", "7bda324c83352c4839ee47e6ff842ed759aaf54b");
 
 console.log('Meow. Is it is me you are looking for?');
 console.log('Kittybot is alive and ready to serve.  Standing by.');
 
 client.room(439862, function(room) {
+  // room.join();
+  // room.speak("Hello.  Is it me you are looking for?");
   room.listen(function(message)  {
     /* Welcome */
     if (message.type === "EnterMessage") {
@@ -14,8 +16,15 @@ client.room(439862, function(room) {
       });
     }
 
+    /* That's what she said */
+    var lastUserMessage = client.user(message.userId, function(user) {
+      if (user.name === "Matthew Sneeden") {
+        room.speak("That's what she said.");
+      }
+    });
+
     /* Bot requests */
-    if (message.body === "/bot") {i
+    if (message.body === "/bot") {
       room.speak("Yes?");
       room.play("rimshot");
       console.log("Someone made a request to the bot.");
