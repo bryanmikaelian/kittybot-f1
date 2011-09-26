759cc19a407d850c125c97909e79e4230a5542cf
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2011-09-26 14:45:53 -0500

    Typo

--- kitty.js
+++ kitty.js
@@ -2,7 +2,7 @@ var http = require('http');
 var client = require('ranger').createClient("fellowshiptech", "52d397ade97cfbfc96d58b50a00996842d785cd7");
 
 console.log('Meow. Is it is me you are looking for?');
-console.log('Kittybot is alive and ready to server.  Standing by.');
+console.log('Kittybot is alive and ready to serve.  Standing by.');
 
 client.room(439862, function(room) {
   room.listen(function(message)  {
