22db8562d0a62ab6204c262e12aec7f77343e24d
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2011-09-26 13:31:20 -0500

    Logging for some port issues

--- kitty.js
+++ kitty.js
@@ -1,6 +1,8 @@
 var http = require('http');
 var client = require('ranger').createClient("fellowshiptech", "52d397ade97cfbfc96d58b50a00996842d785cd7");
 
+console.log(process.env.PORT || 3000);
+
 console.log('Meow. Is it is me you are looking for?');
 client.room(439862, function(room) {
   room.listen(function(message)  {
