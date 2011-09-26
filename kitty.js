c96abc2a8615620502f42ab560d22780d3e4a8d9
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2011-09-26 11:18:18 -0500

    Cleaned up some useless code due to a rewrite of Kittybot

--- kitty.js
+++ kitty.js
@@ -1,27 +1,14 @@
-// var http, ranger, server, client;
-// http = require('http') 
-// ranger = require('ranger');
-// server = http.createServer(function(req, res) {
-//   res.writeHead(200, {
-//     'Content-Type': 'text/plain'
-//   });
-//   return res.end("Meow.\n");
-// });
-
-// port = process.env.PORT || 3000;
-// server.listen(port);
-// setTimeout(function() {
-//   client = ranger.createClient("fellowshiptech", "52d397ade97cfbfc96d58b50a00996842d785cd7");
-//   client.room(438825, function(room) {
-//     room.join(function() {
-//       room.listen(function() {
-//         console.log("Kittybot is listening in the room: " + room.name);
-//       });
-//     });
-//   });
-// }, 1000);
+var http = require('http');
 
 console.log('Meow. Is it is me you are looking for?');
 
+server = http.createServer(function(req, res) {
+  res.writeHead(200, {
+    'Content-Type': 'text/plain'
+  });
+  return res.end("Meow\n");
+});
 
+port = process.env.PORT || 3000;
+server.listen(port);
 
