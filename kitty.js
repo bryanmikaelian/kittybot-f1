1e6936a0d16f2d07f426efaa0fea944cffef6206
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2011-09-26 14:11:19 -0500

    Creating a basic server

--- kitty.js
+++ kitty.js
@@ -1,7 +1,11 @@
 var http = require('http');
+var net = require('net');
 var client = require('ranger').createClient("fellowshiptech", "52d397ade97cfbfc96d58b50a00996842d785cd7");
 
-console.log(process.env.PORT || 3000);
+var server = net.createServer(function (stream) {
+});
+
+server.port = process.env.PORT || 3000;
 
 console.log('Meow. Is it is me you are looking for?');
 client.room(439862, function(room) {
