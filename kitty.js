3f2ef5f9440ea6b34aecaf51a99754745b1671cb
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2011-09-26 14:15:09 -0500

    Reverted changes

--- kitty.js
+++ kitty.js
@@ -1,18 +1,13 @@
 var http = require('http');
-var net = require('net');
 var client = require('ranger').createClient("fellowshiptech", "52d397ade97cfbfc96d58b50a00996842d785cd7");
 
-var server = net.createServer(function (stream) {
-  console.log('Meow. Is it is me you are looking for?');
-  client.room(439862, function(room) {
-    room.listen(function(message)  {
-      if (message.type === "SoundMessage") {
-        console.log("Someone played a sound.");
-      }
-    });
+console.log(process.env.PORT || 3000);
+
+console.log('Meow. Is it is me you are looking for?');
+client.room(439862, function(room) {
+  room.listen(function(message)  {
+    if (message.type === "SoundMessage") {
+      console.log("Someone played a sound.");
+    }
   });
 });
-
-server.port = process.env.PORT || 3000;
-
-
