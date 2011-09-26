ce823c0472950fa3fa6d849a75dcb6ce2f1824b6
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2011-09-26 15:48:24 -0500

    More commands. Rimshot count total. This obviously will only persist as long as the bot is alive.

--- kitty.js
+++ kitty.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var client = require('ranger').createClient("fellowshiptech", "7bda324c83352c4839ee47e6ff842ed759aaf54b");
+var rimshotCount = 0;
 
 console.log('Meow. Is it is me you are looking for?');
 console.log('Kittybot is alive and ready to serve.  Standing by.');
@@ -29,11 +30,21 @@ client.room(439862, function(room) {
       });
     }
 
+    /* Rimshot counts */
+    if (message.type === "SoundMessage" && message.body === "rimshot") {
+      rimshotCount++;
+    }
+
+
     /* Bot requests */
     /* General request, just saying hi */
     if (message.body === "!kitty") {
       room.speak("Hello.  Is it me you are looking for?");
       console.log("Someone made a request to the bot.");
+    }
+
+    if (message.body === "!rcount") {
+      room.speak("Total rimshots played today: " + rimshotCount);
     }
   });
 });
