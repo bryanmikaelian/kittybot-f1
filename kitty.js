a57d94e550ef884b831ce61a4a749bd241594a92
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2011-09-28 16:31:56 -0500

    We need to check and see if we are listening to the room

--- kitty.js
+++ kitty.js
@@ -33,132 +33,134 @@ client.room(439862, function(room) {
   }, 10000);
 
   // Listen to the room
-  room.listen(function(message)  {
-    if (message.body === "/kitty") {
-      room.speak("Meow.");
-      console.log("Kittybot responded to the command /kitty");
-    }
-
-    // Hello, user
-    if (message.type === "EnterMessage") {
-      client.user(message.userId, function(user) {
-        if (user.name === "Kitty Bot") {
+  setInterval(function(){
+    if (!room.isListening()) {
+        room.listen(function(message)  {
+        if (message.body === "/kitty") {
           room.speak("Meow.");
+          console.log("Kittybot responded to the command /kitty");
         }
-        else {
-          console.log(user.name + " connected.");
-          room.speak("Well, how nice of you to join us " + user.name);
+
+        // Hello, user
+        if (message.type === "EnterMessage") {
+          client.user(message.userId, function(user) {
+            if (user.name === "Kitty Bot") {
+              room.speak("Meow.");
+            }
+            else {
+              console.log(user.name + " connected.");
+              room.speak("Well, how nice of you to join us " + user.name);
+            }
+          });
+        }
+
+        // Jenkins queue
+        if (message.body === "/jenkinsq") {
+          var options = {
+            host: 'hudson.dev.corp.local',
+            port: 8080,
+            path: '/queue/api/json'
+          };
+          http.get(options,function(res){
+            res.on('data', function (chunk) {
+              var data = JSON.parse(chunk);
+              if (data['items'].length === 0) {
+                room.speak("There are currently no jobs in the Jenkins queue.");
+              }
+              else {
+                room.speak("Number of jobs in Jenkins queue:  " + data['items'].length);
+              }
+            });
+          });
+          console.log("Kittybot responded to the command /jenkinsq");
+        }
+
+        // Kill kitty
+       if (message.body === "/killkitty") {
+         client.user(message.userId, function(user) {
+            if (user.name === "Bryan Mikaelian") {
+              console.log("Kitty termination has been requested and will be completed.");
+              room.speak("KITTYBOT HAS BEEN MARKED FOR DEATH AND WILL RETURN SHORTLY.  MAKE SENSE?");
+              setTimeout(function(){
+                room.leave();
+                console.log("Kittybot is leaving room " + room.name);
+              }, 5000);
+
+              setTimeout(function(){
+                room.join();
+              }, 10000);
+
+            }
+            else {
+              room.speak("@" + user.name + " Lolz...y u try 2 do dat?");
+            }
+          });
+        };
+
+        // Sifters - Fellowship One Project
+        if (message.body === "/sifters f1") {
+            var options = {
+            host: 'fellowshiptech.sifterapp.com',
+            path: '/api/projects/5348/issues?s=1-2',
+            headers: {'X-Sifter-Token': 'b5c0c1aafc3a4db0d6aa55ed51731bd7'}
+          };
+
+          https.get(options,function(res){
+            res.on('data', function (chunk) {
+              var data = JSON.parse(chunk);
+              var issues = new Array();
+              console.log("A request has been made for all the sifters for the Fellowship One project.");
+              if (data['issues'].length === 0) {
+                room.speak("There are currently no open issues for the Fellowship One project.");
+              }
+              else {
+                for (var i = 0; i < data['issues'].length; i++) {
+                  issues.push("Sifter #" + data['issues'][i]['number']);
+                };
+                room.speak("Total issues for the Fellowship One project: " + issues.length);
+                room.speak("The open issues are: " + issues.join(", "));
+              }
+            });
+          });
+        }
+
+        // Sifters - Configuration Management
+        if (message.body === "/sifters cm") {
+            var options = {
+            host: 'fellowshiptech.sifterapp.com',
+            path: '/api/projects/3624/issues?s=1-2',
+            headers: {'X-Sifter-Token': 'b5c0c1aafc3a4db0d6aa55ed51731bd7'}
+          };
+
+          https.get(options,function(res){
+            res.on('data', function (chunk) {
+              var data = JSON.parse(chunk);
+              var issues = new Array();
+             console.log("A request has been made for all the sifters for the Change Management project.");
+              if (data['issues'].length === 0) {
+                room.speak("There are currently no open change requests.");
+              }
+              else {
+                for (var i = 0; i < data['issues'].length; i++) {
+                  issues.push("Sifter #" + data['issues'][i]['number']);
+                };
+                room.speak("Total change requests open: " + issues.length);
+                room.speak("The current open change requests are: " + issues.join(", "));
+              }
+            });
+          });
+        }
+
+        // That's what she said, Matthew
+        if (message.type === "TextMessage") {
+          client.user(message.userId, function(user) {
+            if (user.name === "Matthew Sneeden") {
+              room.speak("That's what she said.");
+            }
+          });
         }
       });
     }
-
-    // Jenkins queue
-    if (message.body === "/jenkinsq") {
-      var options = {
-        host: 'hudson.dev.corp.local',
-        port: 8080,
-        path: '/queue/api/json'
-      };
-      http.get(options,function(res){
-        res.on('data', function (chunk) {
-          var data = JSON.parse(chunk);
-          if (data['items'].length === 0) {
-            room.speak("There are currently no jobs in the Jenkins queue.");
-          }
-          else {
-            room.speak("Number of jobs in Jenkins queue:  " + data['items'].length);
-          }
-        });
-      });
-      console.log("Kittybot responded to the command /jenkinsq");
-    }
-
-    // Kill kitty
-   if (message.body === "/killkitty") {
-     client.user(message.userId, function(user) {
-        if (user.name === "Bryan Mikaelian") {
-          console.log("Kitty termination has been requested and will be completed.");
-          room.speak("KITTYBOT HAS BEEN MARKED FOR DEATH AND WILL RETURN SHORTLY.  MAKE SENSE?");
-          setTimeout(function(){
-            room.leave();
-            console.log("Kittybot is leaving room " + room.name);
-          }, 5000);
-
-          setTimeout(function(){
-            room.join();
-          }, 10000);
-
-        }
-        else {
-          room.speak("@" + user.name + " Lolz...y u try 2 do dat?");
-        }
-      });
-    };
-
-    // Sifters - Fellowship One Project
-    if (message.body === "/sifters f1") {
-        var options = {
-        host: 'fellowshiptech.sifterapp.com',
-        path: '/api/projects/5348/issues?s=1-2',
-        headers: {'X-Sifter-Token': 'b5c0c1aafc3a4db0d6aa55ed51731bd7'}
-      };
-
-      https.get(options,function(res){
-        res.on('data', function (chunk) {
-          var data = JSON.parse(chunk);
-          var issues = new Array();
-          console.log("A request has been made for all the sifters for the Fellowship One project.");
-          if (data['issues'].length === 0) {
-            room.speak("There are currently no open issues for the Fellowship One project.");
-          }
-          else {
-            for (var i = 0; i < data['issues'].length; i++) {
-              issues.push("Sifter #" + data['issues'][i]['number']);
-            };
-            room.speak("Total issues for the Fellowship One project: " + issues.length);
-            room.speak("The open issues are: " + issues.join(", "));
-          }
-        });
-      });
-    }
-
-    // Sifters - Configuration Management
-    if (message.body === "/sifters cm") {
-        var options = {
-        host: 'fellowshiptech.sifterapp.com',
-        path: '/api/projects/3624/issues?s=1-2',
-        headers: {'X-Sifter-Token': 'b5c0c1aafc3a4db0d6aa55ed51731bd7'}
-      };
-
-      https.get(options,function(res){
-        res.on('data', function (chunk) {
-          var data = JSON.parse(chunk);
-          var issues = new Array();
-          console.log("A request has been made for all the sifters for the Change Management project.");
-          if (data['issues'].length === 0) {
-            room.speak("There are currently no open change requests.");
-          }
-          else {
-            for (var i = 0; i < data['issues'].length; i++) {
-              issues.push("Sifter #" + data['issues'][i]['number']);
-            };
-            room.speak("Total change requests open: " + issues.length);
-            room.speak("The current open change requests are: " + issues.join(", "));
-          }
-        });
-      });
-    }
-
-
-
-    // That's what she said, Matthew
-    if (message.type === "TextMessage") {
-      client.user(message.userId, function(user) {
-        if (user.name === "Matthew Sneeden") {
-          room.speak("That's what she said.");
-        }
-      });
-    }
-  });
+  }, 10000);
 });
 
