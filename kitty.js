6519ca1679e814f957ed59ee988ca30abd694fc2
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2011-09-28 17:05:59 -0500

    trying google analytics

--- kitty.js
+++ kitty.js
@@ -4,6 +4,16 @@ var client = require('ranger').createClient("fellowshiptech", "7bda324c83352c483
 var rimshotCount = 0;
 var roomNumber = 439862;
 
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-26002548-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
 
 console.log('Meow. Is it is me you are looking for?');
 console.log('Kittybot is alive and ready to serve.  Standing by.');
