6409cac75a1554ecdb9b0869a8a0fa7dc515f931
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2011-09-29 09:08:02 -0500

    Some fancier stuff for working with redis in production

--- kitty.js
+++ kitty.js
@@ -1,7 +1,15 @@
 var http = require('http');
 var https = require('https');
 var client = require('ranger').createClient("fellowshiptech", "7bda324c83352c4839ee47e6ff842ed759aaf54b");
-var redis = require('redis'), redisdb = redis.createClient(process.env.REDISTOGO_URL);
+
+if (process.env.REDISTOGO_URL) {
+  var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+  var redis = require("redis").createClient(rtg.port, rtg.hostname);
+  redis.auth(rtg.auth.split(":")[1]);
+} else {
+  var redis = require('redis'), redisdb = redis.createClient(process.env.REDISTOGO_URL);
+}
+
 var roomNumber = 439862;
 var joinRooms = true;
 var kittyInRoom = false;
