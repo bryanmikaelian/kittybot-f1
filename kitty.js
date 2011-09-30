5eeab2f7d87fec189e49f664ba1f879e37357fbd
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2011-09-30 14:31:33 -0500

    Sifter stuff will now move into its own module.

--- kitty.js
+++ kitty.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var https = require('https');
 var client = require('ranger').createClient("fellowshiptech", "7bda324c83352c4839ee47e6ff842ed759aaf54b");
+var sifter = require('./lib/sifter');
 
 if (process.env.REDISTOGO_URL) {
   var rtg   = require("url").parse(process.env.REDISTOGO_URL);
