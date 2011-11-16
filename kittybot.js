2c257357d887473bf87a5029293a81ae2e92dfb2
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2011-11-16 08:46:56 -0600

    Started reworking session to start.

--- kittybot.js
+++ kittybot.js
@@ -8,7 +8,7 @@ var roomNumber = 373588;
 var catNipOn = false;
 var sifterPollerOn = true;
 
-console.log("Starting Kittybot...");
+console.log("Kittybot has been started");
 
 http.createServer(function(req, res) {
   console.log("An HTTP request has been made.");
@@ -18,80 +18,14 @@ http.createServer(function(req, res) {
 
 
 var room = client.room(roomNumber, function(room) {
-
-  // Start the session for the room
-  session.start(room);
+  // Begin session for this room
+  session.init(room);
 
   // Start listening for messages. Check every 2 seconds to see if we are listening.
   var listenInterval = setInterval(function() {
-    if (!room.isListening()) {
-      console.log("Listening to the room " + room.name);
-      room.listen(function(message){
 
-        // Update session as needed
-        if (message.type == "EnterMessage" || message.type == "LeaveMessage") {
-          client.user(message.userId, function (user) { 
-            session.update(message.type, user, function(msg){
-              speak(msg);
-            });
-          });
-        }
+  });
 
-        // Need to check to see if the message is not null since we are using .match
-        if (message.body != null ) {
-
-          // General Commands module
-          kitty.respond(room, message.body, function(response){
-            speak(response);
-          });
-
-          // SoundMessage counts module
-          if (message.type === "SoundMessage") {
-            counts.update(message.body);
-          }
-
-          // Sifters module
-          if (message.body === "/sifters" || message.body === "/crs" || message.body.match(/\/sifter\s+(\d+)/) || message.body.match(/\/cr\s+(\d+)/)) {
-            sifter.processCommand(room, message.body, function(response){
-              speak(response);
-            });
-          }
-
-          // Catnip module
-          if (message.body.match(/\/catnip\s+(on)/)) {
-            console.log("Kittybot is nommin some catnip");
-            catNipOn = true;
-          }
-          if (message.body.match(/\/catnip\s+(off)/)) {
-            console.log("Kittybot has stopped nommin the catnip");
-            catNipOn = false;
-          }
-        }
-      });
-    }
-  }, 2000);
-  // Leet speak module
-  var speak = function(message){
-    if(catNipOn){
-        room.speak(lol.LOLTranslate(message));
-    }
-    else{
-      room.speak(message);
-    }
-  }
-
-  // Sifter API polling module
-  if (sifterPollerOn) {
-    console.log("Polling against the Sifter API is now enabled.");
-    setInterval(function() {
-      sifter.pollAPI(function(issue) {
-        speak(issue['opener_name'] + " has opened the following sifter: Sifter #" + issue['number'] + ": " + issue['subject']);
-      });
-    }, 60000);
-  }
-  else {
-    console.log("Polling against the Sifter API is disabled.");
-  }
 });
 
 
