03c9fd817d5c635c68a52dfb752a2fc49011454b
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2011-11-19 20:05:25 -0600

    Potential workaround for session and kittybot disappearing.  Will confirm after testing.

--- kittybot.js
+++ kittybot.js
@@ -14,26 +14,30 @@ http.createServer(function(req, res) {
   console.log("An HTTP request has been made.");
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end("Meow\n");
-}).listen(Number(process.env.PORT) || 8000);
+}).listen(Number(process.env.PORT) || 8010);
 
 
 var room = client.room(roomNumber, function(room) {
   // Begin session for this room
-  session.init(room, function() {
+  session.init(room);
 
+  // Session has been established.  Start listening to the room
+  room.listen(function(message) {
+
+    // Track when people enter and leave the room.  Upadte session accordingly
+    if ((message.type == "EnterMessage" || message.type == "LeaveMessage") && message.userId !== null) {
+      client.user(message.userId, function (user) { 
+        session.update(room, message.type, user);
+      });
+    }
   });
+
+  // Sync session to keep the streaming API connection alive.  This happens every 10 minutes
+  setInterval(function() {
+    session.sync(room);
+  }, 600000);
+
 });
 
 
-// Heroku is dumb. Ping the app every 10 minutes to make sure kittybot doesn't die.
-setInterval(function() {
-  var options = {
-    host: "kittybot.herokuapp.com",
-    path: "/"
-  };
-  console.log("Pinging kittybot.herokuapp.com");
-  http.get(options, function(res){
-    res.on('data', function (chunk) {
-    });
-  });
-}, 600000);
+
